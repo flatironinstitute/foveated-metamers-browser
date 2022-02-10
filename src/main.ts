2fc47f16f702132c28089c0b8a36ee103cede435
Elizabeth Lovero <elizabethlovero@gmail.com>
2022-02-10 15:55:06 -0500

    Add variable name.

--- src/main.ts
+++ src/main.ts
@@ -110,7 +110,7 @@ function populate(changed: Field=undefined) {
     const row = Tab.insertRow(-1);
     row.classList.add('border', 'border-slate-200','p-4');
     for (let f of Fields)
-      let celly = row.insertCell(-1);
+      var celly = row.insertCell(-1);
       celly.innerText = i[f].toString();
       celly.classList.add('p-4', 'text-xs');
     row.onclick = () => viewImage(i);
