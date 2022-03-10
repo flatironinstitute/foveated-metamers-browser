f66e9e0da7f82f0bc66e94195eec1dfb30263c6e
lizlove <elizabethlovero@gmail.com>
2022-03-10 00:10:04 -0500

    Add filter divs to form.

--- src/main.ts
+++ src/main.ts
@@ -206,6 +206,22 @@ function buildTable() {
       "border-gray-200",
       padding
     );
+    filtDiv.innerHTML = `<h3 class="-my-3 flow-root">
+      <button type="button" class="py-3 bg-white w-full flex items-center justify-between text-sm text-gray-400 hover:text-gray-500"
+      name="plusminus">
+        <span class="font-medium text-gray-900">${f.replace('_', "")}</span>
+        <span class="ml-6 flex items-center">
+          <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
+          </svg>
+          <svg class="h-5 w-5 hidden" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <path fill-rule="evenodd" d="M5 10a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1z" clip-rule="evenodd" />
+          </svg>
+        </span>
+      </button>
+    </h3>`
+
+    filterform.appendChild(filtDiv);
 
     // Selection Row
     const sel = document.createElement("select");
