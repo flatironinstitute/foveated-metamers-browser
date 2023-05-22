cdb0a46bddc0cb9e1e7e8335817464ff57617379
Paul Murray <plmrry@gmail.com>
2023-05-22 15:26:31 -0400

    button opens in new window

--- src/lib/Header.tsx
+++ src/lib/Header.tsx
@@ -23,6 +23,7 @@ export default function Header() {
             <a
               type="button"
               href={button_link}
+              target="_blank"
               className="inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               {button_text}
